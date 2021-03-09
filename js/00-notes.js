641d1e8cfbe257ed18b6a50d52dbf3256224ed31
AudriusKru <krutulis.audrius@gmail.cm>
2021-03-09 17:19:36 +0200

    pertrauka

--- js/00-notes.js
+++ js/00-notes.js
@@ -4,3 +4,71 @@
 // funkcijos - aprasome logika, kuria norime perpanaudoti N kartu, tik su galimai skirtingais paramentrais 
 
 // event'ai (event listeners) - skirta stebeti ivykius (click, scroll, keypress ir pan) i kuriuos reikia sureaguoti iskvieciant norima funkcija
+
+
+/*
+console.log();
+
+ 
+// Kintamuju iniciavimo budai:
+ //const - kintamasis, kurio reiksmes negali ir naujo priskirti *
+// let - kintamasi, kurio reiksme gali buti priskirta kiek norite kartu (is naujo)
+ //var - esant galimybei NENAUDOTI!!!!!! nooooo! tevas tevu tevas
+
+ //* - yra papildomu salygu su komplektiniais kintamasiais
+
+
+ 
+
+ const vardas = 'Audrius';
+ console.log (vardas);
+
+ const pasisveikinimas = 'labas rytas, Lietuva!';
+ console.log(pasisveikinimas);
+
+ const pirmasSkaicius = 7;
+ const antrasSkaicius = 77;
+ const suma = pirmasSkaicius + antrasSkaicius;
+
+ console.log (pirmasSkaicius);
+ console.log (antrasSkaicius);
+ console.log (suma);
+
+ console.log (pirmasSkaicius, '+', antrasSkaicius, '=', suma);
+
+ const mathText = pirmasSkaicius + ' + ' + antrasSkaicius + ' = ' + suma;
+ console.log(mathText);
+
+ console.log('-----')
+
+ const petrasName = 'Petras';
+ const age = 99;
+
+   // Petras age is 99.
+ const petrasAge = petrasName + ' age is ' + age + '.';
+ console.log(petrasAge);
+
+console.log('-----------');
+
+ //sarasas (array)
+ const pazymiai = [5,6,7,3,10];
+ console.log(pazymiai);
+
+ const abc = ['a', 'b', 'c', 'd', 'e', 'f'];
+ console.log(abc);
+
+ const pirmasPazymys = pazymiai[0];
+ console.log(pirmasPazymys);
+
+ const antrasPazymys = pazymiai[1];
+ console.log(antrasPazymys);
+ 
+ const pazymiuSuma = pazymiai[0] + pazymiai[1] + pazymiai[2] + pazymiai[3] + pazymiai[4] + pazymiai[5];
+ 
+ const pazymiuKiekis = pazymiai.length;
+ 
+ const pazymiuVidurkis = pazymiuSuma / pazymiuKiekis;
+ console.log('Pazymiu suma:', pazymiuSuma);
+ console.log('Pazymiu kiekis:', pazymiuKiekis);
+ console.log('Pazymiu vidurkis:', pazymiuVidurkis);
+*/
--- js/01-variables.js
+++ js/01-variables.js
@@ -28,14 +28,14 @@ console.log (veikla);
 
 // 3.
 
-const skaiciuSarasas1 = [10, 12, 15, 17, 18];
-console.log (skaiciuSarasas1);
+const sarasas1 = [10, 12, 15, 17, 18];
+console.log (sarasas1);
 
-const skaiciuSaras2 = [1, 4, 2, 7, 9];
-console.log (skaiciuSaras2);
+const sarasas2 = [1, 4, 2, 7, 9];
+console.log (sarasas2);
 
-const skaiciuSarasas3 = [23, 12, 8, 10, 3];
-console.log (skaiciuSarasas3)
+const sarasas3 = [23, 12, 8, 10, 3];
+console.log (sarasas3)
 
 // 4.
 
@@ -49,71 +49,19 @@ const namas = ['Sis', 'namas', 'yra', 'labai', 'grazus'];
 console.log (namas)
 
 
+//        *Veiksmai su kintamaisiais*
 
+// 1.
 
-/*
-console.log();
+const suma = 2 + 3 + 4;
+console.log (suma)
 
- 
-// Kintamuju iniciavimo budai:
- //const - kintamasis, kurio reiksmes negali ir naujo priskirti *
-// let - kintamasi, kurio reiksme gali buti priskirta kiek norite kartu (is naujo)
- //var - esant galimybei NENAUDOTI!!!!!! nooooo! tevas tevu tevas
+// 2.
 
- //* - yra papildomu salygu su komplektiniais kintamasiais
+const zodziuSuma = 'zmogus' + ' ' + 'mokosi' + ' ' + 'programuoti'; 
+console.log (zodziuSuma);
 
+// 3.
 
- 
-
- const vardas = 'Audrius';
- console.log (vardas);
-
- const pasisveikinimas = 'labas rytas, Lietuva!';
- console.log(pasisveikinimas);
-
- const pirmasSkaicius = 7;
- const antrasSkaicius = 77;
- const suma = pirmasSkaicius + antrasSkaicius;
-
- console.log (pirmasSkaicius);
- console.log (antrasSkaicius);
- console.log (suma);
-
- console.log (pirmasSkaicius, '+', antrasSkaicius, '=', suma);
-
- const mathText = pirmasSkaicius + ' + ' + antrasSkaicius + ' = ' + suma;
- console.log(mathText);
-
- console.log('-----')
-
- const petrasName = 'Petras';
- const age = 99;
-
-   // Petras age is 99.
- const petrasAge = petrasName + ' age is ' + age + '.';
- console.log(petrasAge);
-
-console.log('-----------');
-
- //sarasas (array)
- const pazymiai = [5,6,7,3,10];
- console.log(pazymiai);
-
- const abc = ['a', 'b', 'c', 'd', 'e', 'f'];
- console.log(abc);
-
- const pirmasPazymys = pazymiai[0];
- console.log(pirmasPazymys);
-
- const antrasPazymys = pazymiai[1];
- console.log(antrasPazymys);
- 
- const pazymiuSuma = pazymiai[0] + pazymiai[1] + pazymiai[2] + pazymiai[3] + pazymiai[4] + pazymiai[5];
- 
- const pazymiuKiekis = pazymiai.length;
- 
- const pazymiuVidurkis = pazymiuSuma / pazymiuKiekis;
- console.log('Pazymiu suma:', pazymiuSuma);
- console.log('Pazymiu kiekis:', pazymiuKiekis);
- console.log('Pazymiu vidurkis:', pazymiuVidurkis);
-*/
+const sarasas1_verte = sarasas1[1] - sarasas1[2] + sarasas1[3] - sarasas1[4] + sarasas1[5];
+console.log (sarasas1_verte);
