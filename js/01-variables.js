5b5b1035b8e33b71074a54486865b4b630c1c25a
AudriusKru <krutulis.audrius@gmail.cm>
2021-03-09 14:59:10 +0200

    paskaita

---
+++ js/01-variables.js
@@ -0,0 +1,46 @@
+console.log();
+
+ /*
+ Kintamuju iniciavimo budai:
+ const - kintamasis, kurio reiksmes negali ir naujo priskirti *
+ let - kintamasi, kurio reiksme gali buti priskirta kiek norite kartu (is naujo)
+ var - esant galimybei NENAUDOTI!!!!!! nooooo! tevas tevu tevas
+
+ * - yra papildomu salygu su komplektiniais kintamasiais
+
+
+ */
+
+ const vardas = 'Audrius';
+ console.log (vardas);
+
+ const pasisveikinimas = 'labas rytas, Lietuva!';
+ console.log(pasisveikinimas);
+
+ const pirmasSkaicius = 7;
+ const antrasSkaicius = 77;
+ const suma = pirmasSkaicius + antrasSkaicius;
+
+ console.log (pirmasSkaicius);
+ console.log (antrasSkaicius);
+ console.log (suma);
+
+ console.log (pirmasSkaicius, '+', antrasSkaicius, '=', suma);
+
+ const mathText = pirmasSkaicius + ' + ' + antrasSkaicius + ' = ' + suma;
+ console.log(mathText);
+
+ console.log('-----')
+
+ //sarasas (array)
+ const pazymiai = [5,6,7,3,10];
+ console.log(pazymiai);
+
+ const abc = ['a', 'b', 'c', 'd', 'e', 'f'];
+ console.log(abc);
+
+ const pirmasPazymys = pazymiai[0];
+ console.log(pirmasPazymys);
+
+ const antrasPazymys = pazymiai[1];
+ 
