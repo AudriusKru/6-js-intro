5b0405bda0ef6be2fd9bd2ef0e4762ac145c5873
AudriusKru <krutulis.audrius@gmail.cm>
2021-03-10 17:48:40 +0200

    naudotini

--- js/03-if.js
+++ js/03-if.js
@@ -7,9 +7,9 @@ if () {} else {}
 
 
 palyginimo operatoriai:
-- visi;
-- naudotini;
-- nenaudotini;
+- visi; <, >, <=, =>, ==(LYGU), !=(NELYGU), ===(tekstas su skaiciais), !==, ...
+- naudotini; <, >, <=, <=, ===, !==
+- nenaudotini; ==, !=
 */
 
 const a = 7;
@@ -34,4 +34,31 @@ if (c < d) {
 } else {
     const cdNeMaziau = `${c} yra NE maziau uz ${d}`;
     console.log(cdNeMaziau);
+}
+
+
+////
+console.log('----------');
+////
+
+const e = 8;
+const f = 9;
+
+if (e > f) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
+}
+
+////
+console.log('----------');
+////
+
+const g = '89';
+const h = 89;
+
+if (g !== h) {
+    console.log('TAIP');
+} else {
+    console.log('NE');
 }
