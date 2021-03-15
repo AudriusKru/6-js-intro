e790fa2cdfe269a38702ca1d90b156437912daa4
AudriusKru <krutulis.audrius@gmail.cm>
2021-03-15 13:10:59 +0200

    update

--- js/07-for.js
+++ js/07-for.js
@@ -12,9 +12,6 @@ const abc = ['r', 'y', 't', 'a', 's'];
 let zodis = '';
 let abcIndex = 0
 
-zodis += abc[abcIndex];
-console.log(zodis);
-
 zodis += abc[abcIndex++];
 zodis += abc[abcIndex++];
 zodis += abc[abcIndex++];
@@ -23,7 +20,39 @@ zodis += abc[abcIndex++];
 
 console.log(zodis);
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i <= 7; i++) {
     console.log(i);
 }
+
+console.log('ciklo pabaiga');
+
+console.log('------');
+
+const abc2 = ['a', 'b', 'c', 'd'];
+
+for ( let i = 0; i < abc2.length; i++ ) {
+    const raides = abc2[i];
+    
+    console.log('abc2');
+}
+
+console.log('------');
+
+const marks = [5, 7, 10, 4, 2, 10];
+let sum = 0;
+
+
+for (let i = 0; i < marks.length; i++) {
+    const pazymys = marks[i];
+    sum += pazymys;
+}
+
+const average = sum / marks.length;
+
+console.log(sum, '/', marks.length, '=', average);
+
+
+
+console.log('------');
+
 
