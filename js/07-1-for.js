cf83e3d5753a44126e3baf1d1b978d00b5b953da
AudriusKru <krutulis.audrius@gmail.cm>
2021-03-15 17:49:46 +0200

    reverse

---
+++ js/07-1-for.js
@@ -0,0 +1,56 @@
+/// Namu darbai:
+
+// 1.a
+
+let suma = 0;
+
+for (let i = 0; i <= 0; i++) {
+    suma +=i;
+}
+console.log('1.A. Suma yra', suma);
+
+// 1.b
+
+for (let i = 0; i <= 4; i++) {
+     suma +=i;
+}
+console.log('1.B. Suma yra', suma);
+
+// 1.c
+
+for (let i = 0; i <= 100; i++) {
+    suma +=i;
+}
+console.log('1.C. Suma yra', suma);
+
+// 1.d
+
+for (let i = 574; i <= 815; i++) {
+    suma +=i;
+}
+console.log('1.D. Suma yra', suma);
+
+// 1.e
+
+for (let i = -50; i <= 50; i++) {
+    suma +=i;
+}
+console.log('1.E. Suma yra', suma);
+
+// 1.f
+
+for (let i = -70; i <= 30; i++) {
+    suma +=i;
+}
+console.log('1.F. Suma yra', suma);
+
+///  2. panaudojant ciklą perrašyti tekstinio tipo kintamųjų reikšmes iš kito galo:
+// pvz.: “abcdef” -> “fedcba”
+
+let raides = 'abcdef';
+let atbulai = "";
+for ( let i = raides.length-1; i >= 0; i-- ) {
+    atbulai += raides [i];
+} 
+console.log('2. Raidės kitaip:', atbulai);
+
