070a28021bbd949a292da07aec59934ee0da1436
AudriusKru <krutulis.audrius@gmail.cm>
2021-03-16 11:17:21 +0200

    liekana

---
+++ js/10-liekana.js
@@ -0,0 +1,10 @@
+// https://docs.google.com/document/d/1UnM5MzO7KPf-atlz3HXPJN8XlaN37nlUJ7fp6GwvIZ8/edit
+
+const nuo = 0;
+const iki = 11;
+const daliklis = 3;
+let kiekis = 0;
+
+for (let i = nuo; i <= iki; i++) {
+    console.log(i, i/daliklis);
+}
