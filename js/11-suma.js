50038a4841836734e10691080b0f7608556356ad
AudriusKru <krutulis.audrius@gmail.cm>
2021-03-16 12:45:04 +0200

    marks

---
+++ js/11-suma.js
@@ -0,0 +1,24 @@
+const a = 8;
+const b = 5;
+
+const c = 99;
+const d = 88;
+
+const e = 54155;
+const f = 89514;
+
+// const s3 = (e + f) * 10;
+// console.log(s);
+
+// function *funkcijos pavadinimas* (logika)
+
+function desimteriopaSuma(pirmas, antras) {
+    const s = (pirmas + antras) * 10;
+    console.log(s);
+}
+
+desimteriopaSuma(a, b);
+console.log('------')
+desimteriopaSuma(c, d);
+console.log('------')
+desimteriopaSuma(e, f);
