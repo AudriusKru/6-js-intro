e7261879caee6af565ea1c329ccb73e5973c9e68
AudriusKru <krutulis.audrius@gmail.cm>
2021-03-17 09:59:47 +0200

    change

--- js/14-1-namie.js
+++ js/14-1-namie.js
@@ -1,26 +1,14 @@
-//[1,-4,7,12]
 /*
-count pasitiveSum = [1,-4,7,12];
 
-function positiveSum([1,-4,7,12]) {
-    let sum = 0;
-    for(let i = 0; i < arr.length; i++) {
-      if(arr[i] > 0) {
-       sum += arr[i];
-     }
-    }
-   return sum;
-   }
+function teigiamiSkaiciai(arr) {
+    var result = 0;
+    for (var i=0; i<arr.length; i++)
+      if(arr[i]>0) {
+      var num = arr[i];
+      result += num;
+      }
+    return result;
+  }
+
+teigiamiSkaiciai(1, -4, 7, 12);
 */
-
-const skaiciai = [1,-4,7,12];
-
-function teigiamiskaiciai(arrejus) {
-    let sum = 0;
-    for (let i = 0; i <= arrejus.lenght; i++) {
-        if(arrejus[i] > 0) {
-            sum += arrejus[i];
-        }
-    }
-    return sum;
-}
