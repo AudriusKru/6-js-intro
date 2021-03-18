24631cf82bad5a7e837331d0f2ba510d9609fbe2
AudriusKru <krutulis.audrius@gmail.cm>
2021-03-18 12:59:14 +0200

    didziausias skaicius sarase, kai saras tik is normaliu skaiciu

--- js/16-bigNum.js
+++ js/16-bigNum.js
@@ -1,20 +1,31 @@
 function bigNum (list) {
     // input validation
+    // isarray !! video paziuret
     if (!Array.isArray(list)) {
         return 'ERROR: netinkamas tipas, turi buti array (sarasas)'
     }
-    if (list === null) {
-        return 'ERROR: netinkama reiksme (null)'
+    if (list.length === 0) {
+        return 'ERROR: array is emty';
     }
+   // if (list === null) {
+ //       return 'ERROR: netinkama reiksme (null)'
 
     // logic
+    let biggest = list[0];
 
+    for (let i=0; i<list.length; i++) {
+        const number = list[i];
+        if (number > biggest) {
+            biggest = number;
+        }
+    }
+// jei nori paskutini rast paskutini rasyt number>=biggest
     // result
 
-    return 0;
+    return biggest;
 }
 
-
+/*
 console.log(bigNum('pomidoras'));
 console.log(bigNum(484562));
 console.log(bigNum(true));
@@ -23,17 +34,12 @@ console.log(bigNum(undefined));
 console.log(bigNum());
 console.log(bigNum(bigNum));
 console.log(bigNum(null));
-
 console.log(bigNum({}));
 console.log(bigNum([]));
+*/
 
-/*
-onsole.log(bigNum([1]), '->', 1 );
+console.log(bigNum([1]), '->', 1);
 console.log(bigNum([ 1, 2, 3 ]), '->', 3);
 console.log(bigNum([ -5, 78, 14, 0, 18]), '->', 78);
 console.log(bigNum([ 69, 69, 69, 69, 66]), '->', 69);
 console.log(bigNum([ -1, -2, -3, -4, -5, -6, -7, -8]), '->', -1);
-console.log(bigNum([])) ;
-*/
-
-
