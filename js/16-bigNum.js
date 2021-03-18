b3fa54b3f8ac79a03abc54eab3eb814204e9ff29
AudriusKru <krutulis.audrius@gmail.cm>
2021-03-18 12:32:36 +0200

    comit

--- js/16-bigNum.js
+++ js/16-bigNum.js
@@ -1,13 +1,39 @@
-function bigNum () {
+function bigNum (list) {
+    // input validation
+    if (typeof list !== 'object') {
+        return 'ERROR: netinkamas tipas, turi buti array (sarasas)'
+    }
+    if (list === null) {
+        return 'ERROR: netinkama reiksme (null)'
+    }
+
+    // logic
+
+    // result
+
     return 0;
 }
 
 
+console.log(bigNum('pomidoras'));
+console.log(bigNum(484562));
+console.log(bigNum(true));
+console.log(bigNum(false));
+console.log(bigNum(undefined));
+console.log(bigNum());
+console.log(bigNum(bigNum));
+console.log(bigNum(null));
+
+console.log(bigNum({}));
+console.log(bigNum([]));
+
+/*
 onsole.log(bigNum([1]), '->', 1 );
 console.log(bigNum([ 1, 2, 3 ]), '->', 3);
 console.log(bigNum([ -5, 78, 14, 0, 18]), '->', 78);
 console.log(bigNum([ 69, 69, 69, 69, 66]), '->', 69);
 console.log(bigNum([ -1, -2, -3, -4, -5, -6, -7, -8]), '->', -1);
-console.log(bigNum('pomidoras' ) );
-console.log(bigNum([]) ;
+console.log(bigNum([])) ;
+*/
+
 
